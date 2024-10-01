417cc9ab815e581bbe9a542f9c3e43706ab86ba5
Felipe Torres Cresto <felipe.t.cresto@gmail.com>
2024-10-01 00:35:09 -0300

    feat: create home component

--- src/app/app.routes.ts
+++ src/app/app.routes.ts
@@ -1,3 +1,12 @@
 import { Routes } from '@angular/router';
+import { HomeComponent } from './components/home/home.component';
 
-export const routes: Routes = [];
+export const routes: Routes = [
+    { path: '', component: HomeComponent, },
+
+    // {
+    //   path: 'landing-page',
+    //   loadChildren: () => import('./pages/application/example-landing-page/tutorial.module')
+    //     .then(mod => mod.TutorialModule)
+    // },
+];
